891e2e2fef6dfa79d9430049b0e7391b5bac3ce1
EvrtLinsDev <138541696+EvrtLinsDev@users.noreply.github.com>
2023-07-03 22:48:17 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- primeiratelasegundafase.c
+++ primeiratelasegundafase.c
@@ -134,6 +134,7 @@ int main()
 		printf ("\t1 Registrar Nomes\n");
 		printf ("\t2 Consultar Nome\n");
 		printf ("\t3 Deletar Nomes\n\n");
+		printf("\t4 Sair do Sistema \n\n");
 		printf ("Opção: ");//fim do menu
 		
 		scanf ("%d", &opcao);//armazenando a escolha do usuario
@@ -153,6 +154,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;//Quebra tudo e volta pro inicio
 			break;
 			
 			default:
